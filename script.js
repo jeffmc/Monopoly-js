5c8533f271559246d79626a167358537d1e5f34c
jeff <10823453+jeffmc@users.noreply.github.com>
2022-06-06 23:58:03 -0700

    Board layout

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const canvas = document.getElementById('viewport');
 const gfx = canvas.getContext('2d');
 gfx.translate(0.5,0.5); // Make lines sharp
+gfx.imageSmoothingEnabled = false;
 
-const GFX_WIDTH = 400, GFX_HEIGHT = 400;
 
 // Init and center monopoly instance
 const mp = new Monopoly(0,0);
-mp.x = Math.round((GFX_WIDTH - mp.width) / 2);
-mp.y = Math.round((GFX_HEIGHT - mp.height) / 2);
 
-mp.draw(gfx);
+window.onload = window.onresize = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    mp.x = Math.round((canvas.width - mp.width) / 2);
+    mp.y = Math.round((canvas.height - mp.height) / 2);
+    console.log(`Canvas size: ${canvas.width}, ${canvas.height}`);
+    repaint();
+};
+
+function repaint() {
+    mp.draw(gfx);
+}
+
+repaint();
--- types.js
+++ types.js
@@ -1,9 +1,10 @@
-const BOARD_SIZE = 370;
-const PROPERTY_HEIGHT = 50;
+const PROPERTY_HEIGHT = 60;
 const PROPERTY_PER_EDGE = 9;
+
+const BOARD_SIZE = PROPERTY_HEIGHT * 2 + PROPERTY_PER_EDGE * 50;
+
 const PROPERTY_WIDTH = (BOARD_SIZE-PROPERTY_HEIGHT*2)
   / PROPERTY_PER_EDGE;
-
 
 const LUCK_CARD_WIDTH = 0;
 
@@ -40,9 +41,109 @@ class Monopoly {
   }
 
   render(gfx) {
+    this.drawBorderline(gfx);
+    this.drawPlots(gfx);
+  }
+
+  drawPlots(gfx) {
+    const PROPS = (PROPERTY_PER_EDGE+1) * 4;
+    for (let i=0;i<PROPS;i++) {
+      this.drawPlot(gfx, i);
+    }
+  }
+
+  drawPlot(gfx,loc) {
+    gfx.save();
+
+    const cornerPlot = loc%(PROPERTY_PER_EDGE+1)==0;
+
+    const pwh = (cornerPlot?PROPERTY_HEIGHT:PROPERTY_WIDTH)/2, phh = PROPERTY_HEIGHT/2;
+    const origin = this.getPlotOrigin(loc);
+    const rot = this.getPlotRotation(loc);
+    gfx.translate(origin.x,origin.y);
+    gfx.rotate(rot);
+
+    gfx.fillStyle = "#2aa";
+    gfx.fillText(loc + (cornerPlot ? "C" : ""),5,12);
+
+    gfx.strokeStyle = "#a22";
+    gfx.beginPath();
+    gfx.moveTo(pwh,phh);
+    gfx.lineTo(pwh+10,phh);
+    gfx.stroke();
+    gfx.strokeStyle = "#2a2";
+    gfx.beginPath();
+    gfx.moveTo(pwh,phh);
+    gfx.lineTo(pwh,phh+10);
+    gfx.stroke();
+
+    gfx.restore();
+  }
+
+  getPlotOrigin(i) {
+    const edgeLoc = i % (PROPERTY_PER_EDGE + 1);
+    const side = Math.floor(i / (PROPERTY_PER_EDGE + 1));
+    const w = this.width, h = this.height;
+    if (edgeLoc == 0) {
+      switch (side) {
+      case 0:
+        return {
+          x: 0,
+          y: 0,
+        };
+      case 1:
+        return {
+          x: w-PROPERTY_HEIGHT,
+          y: 0,
+        };
+      case 2:
+        return {
+          x: w-PROPERTY_HEIGHT,
+          y: h-PROPERTY_HEIGHT,
+        };
+      case 3:
+        return {
+          x: 0,
+          y: h-PROPERTY_HEIGHT,
+        };
+      }
+    }
+    switch (side) {
+    case 0:
+      return {
+        x: PROPERTY_HEIGHT + PROPERTY_WIDTH * edgeLoc,
+        y: PROPERTY_HEIGHT,
+      };
+    case 1:
+      return {
+        x: w-PROPERTY_HEIGHT,
+        y: PROPERTY_HEIGHT + PROPERTY_WIDTH * edgeLoc,
+      };
+    case 2:
+      return {
+        x: w-PROPERTY_HEIGHT - PROPERTY_WIDTH * edgeLoc,
+        y: h-PROPERTY_HEIGHT,
+      };
+    case 3:
+      return {
+        x: PROPERTY_HEIGHT,
+        y: h-PROPERTY_HEIGHT - PROPERTY_WIDTH * edgeLoc,
+      };
+    }
+  }
+  getPlotRotation(i) {
+    const edgeLoc = i % (PROPERTY_PER_EDGE + 1);
+    if (edgeLoc == 0) return 0;
+    const side = Math.floor(i / (PROPERTY_PER_EDGE + 1));
+    return Math.PI * ((side+2)%4) *0.5;
+  }
+
+
+  drawBorderline(gfx) {
     gfx.strokeStyle = "#fff";
     gfx.beginPath();
     const w = this.width, h = this.height;
+
     // Board outline Rect
     gfx.moveTo(0,0);
     gfx.lineTo(w,0);
@@ -77,4 +178,5 @@ class Monopoly {
     
     gfx.stroke();
   }
+
 }
